bfc687a93d641c4d085dfc0dfa3a3bb7de093c05
Nathan Sandberg <nathansandberg@gmail.com>
2018-02-12 14:35:45 -0800

    added constructor function with Products

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+
+
+function Product(name, file) {
+    this.name = name;
+    this.file = file;
+    this.timesShown = 0;
+    this.timesClicked = 0;
+
+}
+
+let bag = new Product('bag', 'images/bag.jpg'); 
+let banana = new Product('banana','images/banana.jpg');
+let bathroom = new Product('bathroom', 'images/bathroom.jpg');
+let boots = new Product('boots','images/boots.jpg');
+let breakfast = new Product('breakfast', 'images/breakfast.jpg');
+let bubblegum = new Product('bubblegum', 'images/bubblegum.jpg');
+let chair = new Product('chair', 'images/chair.jpg');
+let cthulhu = new Product('cthulhu', 'images/cthulhu.jpg');
+let dogDuck = new Product('dog-duck', 'images/dog-duck.jpg');
+let dragon = new Product('dragon', 'images/dragon.jpg');
+let pen = new Product('pen', 'images/pen.jpg');
+let petSweep = new Product('pet-sweep','images/pet-sweep.jpg');
+let scissors = new Product('scissors', 'images/scissors.jpg');
+let shark = new Product('shark', "images/shark.jpg");
+let sweep = new Product('sweep', 'images/sweep.jpg');
+let tauntaun = new Product('tauntaun', 'images/tauntaun.jpg');
+let unicorn = new Product('unicorn', 'images/unicorn.jpg');
+let usb = new Product('usb', 'images/usb.jpg');
+let waterCan = new Product('water-can', 'images/water-can.jpg');
+let wineGlass = new Product('wine-glass', 'images/wine-glass');
+
+
+
+
